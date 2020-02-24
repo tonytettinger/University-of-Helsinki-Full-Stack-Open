fdcfac906138ea912bc50eced16557464ab76bca
Antal Tettinger <antaltettinger@antals-mbp.t.hu>
2020-02-24 10:29:09 +0100

    Part1-courseinfo: Completed

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const Header = (props) => {
+    // const-definitions
+  
+    return (
+    <h1>{props.course.name}</h1>
+    )
+  }
+  const Part = (props) => {
+    // const-definitions
+  
+    return (
+      <>
+        <p>
+        {props.part} {props.exercises}
+        </p>
+      </>
+    )
+  }
+  const Content = (props) => {
+    // const-definitions
+  
+    return (
+      <>
+        <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+        <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+        <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>        
+      </>
+    )
+  }
+
+const Total = (props) => {
+    return (
+      <>
+        <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+      </>
+    )
+  }
+
+  const App = () => {
+    // const-definitions
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
