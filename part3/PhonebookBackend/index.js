d8896e8f0eb2ac76042da0d3fca5c9fb5c234454
tonytettinger <antal.tettinger@gmail.com>
2020-09-18 19:11:21 +0200

    Commit all

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
@@ -103,7 +106,7 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT);
 console.log("server is running on port ", PORT);
