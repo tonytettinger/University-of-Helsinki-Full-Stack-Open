902f5831241cf998b728d402f34f451d68bd7e64
tonytettinger <antal.tettinger@gmail.com>
2020-10-10 13:55:47 +0200

    uibuild

---
+++ part3/PhonebookBackend/build/precache-manifest.70581811614fcae1e75f6aba01b02671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e993f4ab76ab61e5ad601ac9ebee9054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeabc9f93ab7776a6336",
+    "url": "/static/js/2.83ad575d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83ad575d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16c916f216759202cae",
+    "url": "/static/js/main.b092a24d.chunk.js"
+  },
+  {
+    "revision": "f49efa9d212fac417656",
+    "url": "/static/js/runtime-main.a5118a35.js"
+  }
+]);
